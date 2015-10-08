851923ab8837c552ae069eabef50ffae69f15169
ivan <ivan@ivanbook>
2015-10-08 15:10:25 +0300

    Added README and removed unused code.

---
+++ README.md
@@ -0,0 +1,2 @@
+Prototype of priority queue turn-based time system
+See http://www.roguebasin.com/index.php?title=A_priority_queue_based_turn_scheduling_system
--- src/ioroutines/BasicIO.java
+++
@@ -1,9 +0,0 @@
-package ioroutines;
-
-/**
- * used for basic definition of io operations for debug output
- * @author ivan
- */
-public interface BasicIO {
-    
-}
